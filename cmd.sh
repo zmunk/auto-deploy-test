e1b763c68aa1fc984a9f16226f00d6a1d3e8a4de
zmunk <ibrahim.tigrek@gmail.com>
2024-01-30 16:34:55 +0300

    create ec2 instance through cloudformation

---
+++ cmd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+export AWS_DEFAULT_PROFILE=nexus
+
+STACK_NAME="test-stack-1"
+
+if [ "$1 $2" = "create stack" ]; then
+    aws cloudformation create-stack \
+        --on-failure DO_NOTHING \
+        --stack-name "$STACK_NAME" \
+        --template-body "file://template.yaml" \
+        --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+    exit 0
+fi
+
+if [ "$1 $2" = "delete stack" ]; then
+    aws cloudformation delete-stack \
+        --stack-name "$STACK_NAME"
+    exit 0
+fi
+
+if [ "$1 $2" = "stack status" ]; then
+    aws cloudformation describe-stacks \
+        --stack-name "$STACK_NAME" \
+        --query "Stacks[0].StackStatus" \
+        --output text
+    exit 0
+fi
+
+if [ "$1 $2" = "stack logs" ]; then
+    aws cloudformation describe-stack-events \
+        --stack-name "$STACK_NAME" \
+        --query "StackEvents[].[ResourceStatus, ResourceStatusReason]" \
+        --output text
+    exit 0
+fi
+
+echo "invalid arguments: $@"
+
+exit 1
